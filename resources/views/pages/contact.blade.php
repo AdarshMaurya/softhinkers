4c34733cc79fc8be935cc1edf9a3880449a81eb2
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-07-16 19:37:26 +0530

    - edited placeholder for contacts

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -10,17 +10,18 @@
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label name="email">Email:</label>
-                    <input id="email" name="email" class="form-control">
+                    <input id="email" name="email" class="form-control" placeholder="Please enter your email">
                 </div>
 
                 <div class="form-group">
                     <label name="subject">Subject:</label>
-                    <input id="subject" name="subject" class="form-control">
+                    <input id="subject" name="subject" class="form-control" placeholder="Your subject here">
                 </div>
 
                 <div class="form-group">
                     <label name="message">Message:</label>
-                    <textarea id="message" name="message" class="form-control">Type your message here...</textarea>
+                    <textarea id="message" name="message" class="form-control"
+                              placeholder="Type your message here..."></textarea>
                 </div>
 
                 <input type="submit" value="Send Message" class="btn btn-success">
